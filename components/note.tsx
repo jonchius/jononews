43a4f78dd206877a2cffcf76f2b899b82ac67f13
jonchius <jonchius@gmail.com>
2025-03-05 12:34:49 -0500

    updating alert note

--- components/note.tsx
+++ components/note.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link"
 export default function Note() {
   return (
-    <aside className="border-2 border-gray-300 p-5 my-5">⚠️ Note: the following articles may contain various amounts of copium and Trump Derangement Syndrome (TDS) - viewers discretion is advised!</aside>
+    <aside className="border-2 border-gray-300 p-5 my-5">⚠️ Note: the following articles appear via the <Link href="https://news.ycombinator.com/" target="_blank">ycombinator.com</Link> API - none are fully endorsed by me!</aside>
   )
 }
