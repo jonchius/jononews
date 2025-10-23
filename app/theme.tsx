5578cb703a96b7474e7857fb3588944abd49ea43
jonchius <jonchius@gmail.com>
2025-10-23 16:55:51 -0400

    removing old theme-toggler

---
+++ app/theme.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import { text } from "../components/text"
+
+export function Theme() {
+  useEffect(() => {
+    const theme = localStorage.getItem("theme") || "light"
+    document.documentElement.className = theme
+    document.documentElement.style.colorScheme = theme   
+  }, [])
+
+  return null
+}
+
+export function ThemeToggle() {
+
+  const [toggle, setToggle] = useState(localStorage.getItem("theme"))
+
+  const toggleTheme = () => {
+    const html = document.documentElement
+    const newTheme = html.classList.contains('dark') ? 'light' : 'dark'
+    setToggle(newTheme)
+    html.className = newTheme
+    html.style.colorScheme = newTheme
+    localStorage.setItem("theme", newTheme)
+  }
+
+  return (
+    <div>
+      <button onClick={toggleTheme}>      
+        <span aria-hidden="true" className="mr-2">{toggle === 'dark' ? '🌞' : '🌙'}</span>
+        {toggle === 'dark' ? text['switch to light mode'] : text['switch to dark mode']}
+      </button>
+    </div>
+  )
+}
