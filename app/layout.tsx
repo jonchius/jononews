86dfbbb0b01016dad6832527518dbc687620b575
jonchius <jonchius@gmail.com>
2025-10-23 17:35:58 -0400

    updating dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,16 +10,28 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
+  const setInitialTheme = `
+    (function() {
+      try {
+        const theme = localStorage.getItem('theme')      
+        document.documentElement.setAttribute('data-theme', theme)
+        document.documentElement.style.colorScheme = theme
+        if (theme === 'dark') document.documentElement.classList.add('dark')
+        else document.documentElement.classList.remove('dark')
+      } catch (e) {}
+    })()`
+
   return (
-    <html lang="en" data-arp="">
+    <html lang="en" data-arp="" suppressHydrationWarning>
       <head>
-        <link rel="shortcut icon" href={`/favicon.png?v=${Date.now()}`} />        
+        <link rel="shortcut icon" href={`/favicon.png?v=${Date.now()}`} />
+        <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
       </head>
       <body>
         <Theme />
-        <Head />                
-        {children}        
-        <Tail />      
+        <Head />
+        {children}
+        <Tail />
       </body>
     </html>
   )
