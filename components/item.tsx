5a74bc626057fa49e8d4526a0038920a2face448
jonchius <jonchius@gmail.com>
2025-10-24 18:51:07 -0400

    updating item aesthetic

--- components/item.tsx
+++ components/item.tsx
@@ -7,6 +7,7 @@ news item template
 'use client'
 
 import Link from "next/link"
+import { text } from "./text"
 
 export interface ItemProps {
   item: {
@@ -34,12 +35,12 @@ export default function Item({item}: ItemProps) {
         <Link href={url} target="_blank">{title}</Link>
       </h3>
 
-      <aside className="item-meta text-gray-700 dark:text-gray-200 text-sm flex gap-2">
-        <span className="item-date font-bold mr-1">{created_at.substring(0,10)} </span>
-        <span className="item-time">at {`${created_at.substring(11,16)} UTC`} </span>
-        <span className="item-skor">({points} pts) </span>
-        <span className="item-host text-gray-400">via {getURLHost(url)}</span>
-        <span className="item-comm"><Link className="text-green-600" href={`https://news.ycombinator.com/item?id=${objectID}`} target="_blank">comments</Link></span>
+      <aside className="item-meta text-gray-700 dark:text-gray-200 text-sm flex gap-1">
+        <span className="item-date font-bold">{created_at.substring(0,10)} </span>
+        <span className="item-time">@ {`${created_at.substring(11,16)} UTC`} </span>
+        <span className="item-vote">({points} {text['points']}) </span>
+        <span className="item-host text-gray-400 mx-2">via {getURLHost(url)}</span>
+        <span className="item-comm"><Link className="text-green-600" href={`https://news.ycombinator.com/item?id=${objectID}`} target="_blank">{text['comments']}</Link></span>
       </aside>
 
     </article>
